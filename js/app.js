2e693485c29c1ad06bb4a5d71e064ae3871107b2
Holardeeps <praiseelijah2001@gmail.com>
2024-03-21 17:24:55 +0100

    Added the Cart Section

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 // Login form on click
 let loginForm = document.querySelector(".header .login-form");
+let cart = document.querySelector(".header .cart");
 
 document.querySelector("#user-btn").onclick = () =>{
     loginForm.classList.toggle("active");
+};
+
+document.querySelector("#cart-btn").onclick = () =>{
+    cart.classList.toggle("active");
 };
 
 
