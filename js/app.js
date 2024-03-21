bfbb2a41512f83e7276cd23080d0be80cf36b4af
Holardeeps <praiseelijah2001@gmail.com>
2024-03-21 14:56:00 +0100

    Added the login drop-down

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+// Login form on click
+let loginForm = document.querySelector(".header .login-form");
+
+document.querySelector("#user-btn").onclick = () =>{
+    loginForm.classList.toggle("active");
+};
+
+
+
+
+
 var swiper = new Swiper(".hero-slider", {
     loop: true,
     direction: "horizontal",
